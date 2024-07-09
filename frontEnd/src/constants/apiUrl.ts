b71117e6d5dae226717204de36c9163d072118c8
phm6530 <squirrel309@naver.com>
2024-07-09 18:47:23 +0900

    pageSpeed - Font woff2 추가로 성능 최적화

--- frontEnd/src/constants/apiUrl.ts
+++ frontEnd/src/constants/apiUrl.ts
@@ -1,8 +1,8 @@
 // dev
-export const ENDPOINT_URL = 'http://localhost:8080';
+// export const ENDPOINT_URL = 'http://localhost:8080';
 
 // prod S3 storate
-// export const ENDPOINT_URL = 'https://api.h-creations.com/api';
+export const ENDPOINT_URL = 'https://api.h-creations.com/api';
 export const IMG_URL = `https://d33h8icwcso2tj.cloudfront.net`;
 
 export const IMG_UPLOAD_PATH = 'uploads';
